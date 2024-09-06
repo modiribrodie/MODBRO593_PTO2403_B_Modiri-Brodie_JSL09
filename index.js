ff51bf3de230fdf9d8494e629dd91a559681c033
modiribrodie <163874746+modiribrodie@users.noreply.github.com>
2024-09-06 20:18:35 +0200

    styled author info
    Styled the text of the author name to look more presentable

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.full})`
-		document.getElementById("author").textContent = data.user.name
+		document.getElementById("author").textContent = `By: ${data.user.name}`
     })
     
 /**
